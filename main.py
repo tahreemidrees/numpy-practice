456baf899d420404321932f95c5e912c263b8ed8
Tahreem <tumharaemail@example.com>
2026-04-09 20:03:30 +0500

    Add 1D and 2D arrays

--- main.py
+++ main.py
@@ -9,3 +9,11 @@ arr = np.array([1, 2, 3, 4, 5])
 print("Array:", arr)
 print("Shape:", arr.shape)
 print("Dimensions:", arr.ndim)
+import numpy as np
+
+arr1 = np.array([1, 2, 3])
+arr2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print("1D:", arr1)
+print("2D:")
+print(arr2)
