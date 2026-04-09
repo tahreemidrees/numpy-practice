60fba196af9a5c663f9fc26395a12a7466e47df3
Tahreem <tumharaemail@example.com>
2026-04-09 20:46:13 +0500

    Add reshape example

--- main.py
+++ main.py
@@ -24,3 +24,9 @@ arr = np.array([10, 20, 30, 40, 50])
 print(np.sum(arr))
 print(np.mean(arr))
 print(np.max(arr))
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5, 6])
+reshaped = arr.reshape(2, 3)
+
+print(reshaped)
