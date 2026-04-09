c82b0100ac9dbf94a6b4a153810881eec08beb46
Tahreem <tumharaemail@example.com>
2026-04-09 20:45:30 +0500

    Add sum mean and max functions

--- main.py
+++ main.py
@@ -17,3 +17,10 @@ arr2 = np.array([[1, 2, 3], [4, 5, 6]])
 print("1D:", arr1)
 print("2D:")
 print(arr2)
+import numpy as np
+
+arr = np.array([10, 20, 30, 40, 50])
+
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.max(arr))
